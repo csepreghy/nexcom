73c0a8c4418da50fa59a5b07645c0a63ebe5aeea
Andrew <andris788@gmail.com>
2020-10-07 14:44:54 +0200

    (preprocessing): added config

---
+++ preprocessing/config.py
@@ -0,0 +1,8 @@
+vocab_size = 1000
+maxlen = 1000
+batch_size = 32
+embedding_dims = 10
+filters = 16
+kernel_size = 3
+hidden_dims = 250
+epochs = 3
--- preprocessing/load_text.py
+++ preprocessing/load_text.py
@@ -2,6 +2,8 @@ from os import listdir
 from os.path import isfile, join
 import pandas as pd
 
+import preprocessing.config as config
+
 def load_text():
     df_raw_data = pd.read_csv('data/Exercise STUDENT Training dataset - 1X IRI 7 Oct 20.csv')
     print(f'{df_raw_data}')
