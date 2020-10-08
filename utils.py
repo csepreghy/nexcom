adae1cfbb788a263e27b8e808edf8774a80d9626
Andrew <andris788@gmail.com>
2020-10-08 13:17:55 +0200

    (nn): craeted utils file with model evaluation

---
+++ utils.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+
+from sklearn.metrics import f1_score
+
+def evaluate_model(model, X_train, y_train, X_test, y_test):
+    _, train_acc = model.evaluate(X_train, y_train)
+    _, test_acc = model.evaluate(X_test, y_test)
+
+    print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
+
+    y_pred = model.predict(X_test)
+    y_pred = tf.one_hot(tf.math.argmax(y_test, axis=1), depth=y_pred.shape[1])
+    
+    f1 = f1_score(y_test, y_pred, average='weighted')
+
+    print(f'Weighted f1 score: {f1}')
+
+    return train_acc, test_acc, f1
