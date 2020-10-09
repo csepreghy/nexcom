6e26f1bae14c7c4780bcb31202077a6caf4fcb03
Andrew <andris788@gmail.com>
2020-10-09 10:22:21 +0200

    (nn): better separated cnn and lstm

--- train.py
+++ train.py
@@ -4,14 +4,20 @@ from neural_networks.lstm import LSTM
 from preprocessing import load_text, run_preprocessing
 from neural_networks.utils import evaluate_model
 
-if __name__ == '__main__':
-    df = load_text(max_len=-1)
+def run_cnn(df):
     cnn = CNN(config)
     X_train, X_test, X_val, y_train, y_test, y_val = cnn.prepare_data(df)
     model = cnn.fit(X_train, X_test, X_val, y_train, y_test, y_val)
     evaluate_model(model, X_train, y_train, X_test, y_test)
 
-    # lstm = LSTM(config)
-    # X_train, X_test, X_val, y_train, y_test, y_val = lstm.prepare_data(df)
-    # model = lstm.fit(X_train, X_test, X_val, y_train, y_test, y_val)
-    # evaluate_model(model, X_train, y_train, X_test, y_test)
+def run_lstm(df):
+    lstm = LSTM(config)
+    X_train, X_test, X_val, y_train, y_test, y_val = lstm.prepare_data(df)
+    model = lstm.fit(X_train, X_test, X_val, y_train, y_test, y_val)
+    evaluate_model(model, X_train, y_train, X_test, y_test)
+
+if __name__ == '__main__':
+    df = load_text(max_len=-1)
+    # run_cnn(df)
+    run_lstm(df)
+    
