57834e62f7dc8873857e6aea93fe89b211a3e9f1
Andrew <andris788@gmail.com>
2020-10-11 12:16:35 +0200

    (cnn): removed unused parameters

--- neural_networks/cnn.py
+++ neural_networks/cnn.py
@@ -86,7 +86,7 @@ class CNN():
         
         return [earlystopping, modelcheckpoint]
 
-    def fit(self, X_train, X_test, X_val, y_train, y_test, y_val):
+    def fit(self, X_train, X_test, X_val, y_train, y_test):
         model = self._build_model(self.config)
 
         callbacks = self._get_callbacks(self.config)
